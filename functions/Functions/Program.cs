3be2497d1330946e64af3187d7212850c4d68dbd
Opilane TTHK <opilane@tthk.ee>
2019-10-17 17:05:19 +0300

    files

---
+++ functions/Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Hello();
+        }
+       /* public static Double myFunction ()
+        {
+
+            double myResult = 0;
+            return myResult;
+        }*/
+        public static void Hello ()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public static void GreetByName ( string name)
+        {
+        Console.WriteLine($"Hello {name}");
+        }
+    }
+}
